7294c495086b57c3bdd8e36ea0c906c80ec1df21
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-07-21 01:38:08 -0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -21,8 +21,8 @@ global timeArray, gait, R
 global unwrapT, unwrapS, unwrapH
 
 
-ip = "localhost"
-port = 6565
+ip = "10.0.0.24"
+port = 7000
 
 calibTime = 2
 
@@ -75,10 +75,10 @@ qFlagDict = {
 }
 
 addressDict = {
-	"12": "rThigh",
+	"22": "rThigh",
 	"11": "rShank",
 	"10": "rHeel",
-	"20": "lowBack",
+	"30": "lowBack",
 }
 
 orderDict = {
@@ -146,8 +146,8 @@ R = np.array([[25/10,0,0], [0,0.00001*5, 0],[0,0,0.0001*5]])
 
 fileDump = open("algDump.txt", "w+")
 fileOGDump = open("algDump_nofilter.txt", "w+")
-fileDump.write("q_s | d_q_s | dd_x_s | dd_y_s | q_h | d_q_h | dd_x_h | dd_y_h")
-fileOGDump.write("q_s | d_q_s | dd_x_s | dd_y_s | q_h | d_q_h | dd_x_h | dd_y_h")
+fileDump.write("q_s d_q_s dd_x_s dd_y_s q_h d_q_h dd_x_h dd_y_h\n")
+fileOGDump.write("q_s d_q_s dd_x_s dd_y_s  q_h d_q_h dd_x_h dd_y_h\n")
 
 
 class KalmanFilter(object):
