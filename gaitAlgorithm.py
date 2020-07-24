99389f52202c925a44c49db8671f0c6b5892677c
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-07-24 15:40:09 -0400

    Create gaitAlgorithm.py

---
+++ gaitAlgorithm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class gaitDetect:
+    __init__(self, firstVar):
+        self.firstVar = firstVar
+        
+        
+
+if __name__ == "__main__":
+    #Take Input
+    #Set input to variables gyZ10, gyZ11
+    #if first run
+        #create object to store, iitialize with first number
+    #else
+        #add 
