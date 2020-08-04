3cc0493abf0072cb59aff7a446a022e928963a80
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-08-03 23:01:36 -0400

    Update algorithm3.py

--- algorithm3.py
+++ algorithm3.py
@@ -263,9 +263,14 @@ def package_handler_raw(tup):
     
     out = []
     
-    for x in tup:
-        out.append(x)
-    
+    for pos, x in enumerate(tup):
+        if pos < 3:
+            out.append(x * .07)
+        elif pos >=3 and pos < 6:
+            out.append(x * .000244)
+        elif pos >= 6:
+            out.append(x * .00014)
+            
     return out
 
     
