bc4c78e06b8036d5f77b0ee2dbe03f752819eea8
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-08-03 22:44:41 -0400

    Update algorithm3.py

--- algorithm3.py
+++ algorithm3.py
@@ -395,8 +395,11 @@ def data_handler(address, *args):
         gaitDetectShank.angleCalc(gaitDetectShank)
         gaitDetectHeel.angleCalc(gaitDetectShank)
         
-        dd_q_t = gaitDetectThigh.angularAcceleration() 
-        dd_q_s = gaitDetectShank.angularAcceleration()
+	gaitDetectThigh.angularAcceleration()
+	gaitDetectShank.angularAcceleration()
+	
+        dd_q_t = gaitDetectThigh.angularAcceleration
+        dd_q_s = gaitDetectShank.angularAcceleration
 
         outputString = f"{timeToRun} {gaitDetectShank.gaitStage} {gaitDetectThigh.zAngle} {gaitDetectThigh.gyZ} {gaitDetectShank.zAngle} {gaitDetectShank.gyZ} {gaitDetectHeel.zAngle} {gaitDetectHeel.gyZ}"
         print(outputString)
